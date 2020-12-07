0ee8efe3d2451283516c606c1e20bf068a247506
giiaga <ghousali99@gmail.com>
2020-12-07 10:28:51 -0500

    heroku login adjustmentd

--- public/javascripts/interactivity.js
+++ public/javascripts/interactivity.js
@@ -5,7 +5,7 @@ window.addEventListener('DOMContentLoaded', () => {
   Element.prototype.appendChildren = function (...children) {
     children.forEach(child => this.appendChild(child));
   };
-//k
+//kk
   // Enable upvote/downvote buttons
   document.querySelectorAll('.votingbutton').forEach(button => {
     button.addEventListener('click', voter);
